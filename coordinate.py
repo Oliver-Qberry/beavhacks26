87f2caaaec67a7bd6e3a13308bfffdd277d757d1
Oliver-Qberry <162727806+Oliver-Qberry@users.noreply.github.com>
2026-05-02 15:40:19 -0700

    added coordinate class

---
+++ coordinate.py
@@ -0,0 +1,10 @@
+class Coordinate:
+    x: int
+    y: int
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def print(self) -> None:
+        print(f"({self.x}, {self.y})")
