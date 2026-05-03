62895f3d8f4704e7b6e05384b2147ad123c1fe1a
Oliver-Qberry <162727806+Oliver-Qberry@users.noreply.github.com>
2026-05-02 17:12:57 -0700

    Wink detection

--- eyetracking_testing.py
+++ eyetracking_testing.py
@@ -5,9 +5,14 @@ import time
 import os
 import pyautogui
 
+from controls.mouse import left_click, right_click, scroll, move_mouse
+
 from coordinate import Coordinate
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+
+WINK_THRESHOLD = 0.09
+
 
 calibrating = False
 screen_width, screen_height = pyautogui.size()
@@ -15,12 +20,15 @@ screen_width, screen_height = pyautogui.size()
 left_calibration = []
 right_calibration = []
 
-"""def calculate_calibration():
-    left_eye = (left_calibration[-1][0] + left_calibration[-1][1]) / 2
-    right_eye = (right_calibration[-1][0] + right_calibration[-1][1]) / 2
+def calculate_EAR(landmarks, p1: int, p2: int, p3: int, p4:int) -> float:
+    left = landmarks[p1]
+    right = landmarks[p4]
+    top = landmarks[p2]
+    bottom = landmarks[p3]
 
-    #u =
-    pass"""
+    horizontal = ((left.x - right.x) ** 2 + (left.y - right.y) ** 2) ** 0.5
+    vertical = ((top.x - bottom.x) ** 2 + (top.y - bottom.y) ** 2) ** 0.5
+    return vertical / horizontal
 
 def to_pixel(landmark):
     return int(landmark.x * w), int(landmark.y * h)
@@ -35,6 +43,10 @@ def get_center(indices, landmarks):
 def start_calibration() -> bool:
     print("Starting calibration...")
     return True
+
+def input_handling(key):
+    pass
+
 
 
 # --- Load model ---
@@ -100,16 +112,16 @@ while True:
             cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)"""
 
         # Edges of left iris
-        """for landmark in LEFT_IRIS:
+        for landmark in LEFT_IRIS:
             x = int(result.face_landmarks[0][landmark].x * w)
             y = int(result.face_landmarks[0][landmark].y * h)
-            cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)"""
+            cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
 
         # edges of right iris
-        """for landmark in RIGHT_IRIS:
+        for landmark in RIGHT_IRIS:
             x = int(result.face_landmarks[0][landmark].x * w)
             y = int(result.face_landmarks[0][landmark].y * h)
-            cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)"""
+            cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
 
         # corners of the left eye
         for landmark in LEFT_EYE_CORNERS:
@@ -118,14 +130,19 @@ while True:
             cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
 
         # corners of the right eye
-        """for landmark in RIGHT_EYE_CORNERS:
+        for landmark in RIGHT_EYE_CORNERS:
             x = int(result.face_landmarks[0][landmark].x * w)
             y = int(result.face_landmarks[0][landmark].y * h)
-            cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)"""
+            cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
 
         #left_iris_center_x, left_iris_center_y = get_center(LEFT_IRIS,result.face_landmarks[0])
-        """cv2.circle(frame, (get_center(LEFT_IRIS,result.face_landmarks[0])), 1, (0, 255, 0), -1)
-        cv2.circle(frame, (get_center(RIGHT_IRIS, result.face_landmarks[0])), 1, (0, 255, 0), -1)"""
+        cv2.circle(frame, (get_center(LEFT_IRIS,result.face_landmarks[0])), 1, (0, 255, 0), -1)
+        cv2.circle(frame, (get_center(RIGHT_IRIS, result.face_landmarks[0])), 1, (0, 255, 0), -1)
+
+    #if result.face_landmarks[0][159].x - result.face_landmarks[0][145].x <:
+    cv2.circle(frame, (int(result.face_landmarks[0][159].x * w), int(result.face_landmarks[0][159].y * h)), 1, (0, 255, 0), -1)
+    cv2.circle(frame, (int(result.face_landmarks[0][145].x * w), int(result.face_landmarks[0][145].y * h)), 1, (0, 255, 0), -1)
+    #print(f"Left eyelid distance: {result.face_landmarks[0][159].x - result.face_landmarks[0][145].x}")
 
 
     # Show window
@@ -150,7 +167,17 @@ while True:
         print("u: ", u)"""
 
         #print(f"x position: {u *screen_width}")
-        pyautogui.moveTo(u * screen_width, v * screen_height)
+        move_mouse(u * screen_width, v * screen_height)
+    left_EAR = calculate_EAR(result.face_landmarks[0], 33, 159, 145, 133)
+    right_EAR = calculate_EAR(result.face_landmarks[0], 362, 386, 374, 263)
+    #TODO: add bool to not have it continually wink
+    if left_EAR < WINK_THRESHOLD < right_EAR:
+        print("left wink")
+        left_click()
+    elif left_EAR > WINK_THRESHOLD > right_EAR:
+        print("right wink")
+        right_click()
+
 
     key = cv2.waitKey(1) & 0xFF
     # Press ESC to quit
