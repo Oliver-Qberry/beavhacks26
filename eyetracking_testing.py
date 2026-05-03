780282068d6f5e121cfc5652baf73734eaf047b2
Oliver-Qberry <162727806+Oliver-Qberry@users.noreply.github.com>
2026-05-02 20:37:43 -0700

    eye tracking clean up

--- eyetracking_testing.py
+++ eyetracking_testing.py
@@ -24,6 +24,8 @@ RIGHT_IRIS = [474, 475, 476, 477]
 LEFT_IRIS = [469, 470, 471, 472]
 LEFT_EYE_CORNERS = [33, 133]
 RIGHT_EYE_CORNERS = [362, 263]
+LEFT_EYE_LIDS = [145, 133]
+RIGHT_EYE_LIDS = [374, 263]
 
 EYE_MARKERS = [RIGHT_IRIS, LEFT_IRIS, LEFT_EYE_CORNERS, RIGHT_EYE_CORNERS]
 
@@ -36,8 +38,6 @@ SMOOTHING = 0.5
 # -----Calibration-----
 DIRECTIONS = ["top left", "top right", "bottom left", "bottom right"]
 
-left_calibration = []
-right_calibration = []
 avg_calibration = []
 
 
@@ -48,11 +48,11 @@ def avg_coords(coord_1: Coordinate, coord_2: Coordinate) -> Coordinate:
 
 
 # Calculates the Eye Aspect Ratio (EAR) or how open or closed the eye is, using the provided landmarks
-def calculate_EAR(landmarks, p1: int, p2: int, p3: int, p4:int) -> float:
-    left = landmarks[p1]
-    right = landmarks[p4]
-    top = landmarks[p2]
-    bottom = landmarks[p3]
+def calculate_EAR(landmarks, corners, eyelids) -> float:
+    left = landmarks[corners[0]]
+    right = landmarks[eyelids[1]]
+    top = landmarks[corners[1]]
+    bottom = landmarks[eyelids[0]]
 
     horizontal = ((left.x - right.x) ** 2 + (left.y - right.y) ** 2) ** 0.5
     vertical = ((top.x - bottom.x) ** 2 + (top.y - bottom.y) ** 2) ** 0.5
@@ -73,10 +73,6 @@ def start_calibration() -> bool:
     print("Starting calibration...")
     print(f"Please look to the {DIRECTIONS[0]}")
     return True
-
-# Does absolutely nothing because I don't feel like dealing with all the variables that need to be passed around
-def input_handling(key):
-    pass
 
 def create_detector():
     model_path = os.path.join(os.path.dirname(__file__), "face_landmarker.task")
@@ -101,13 +97,44 @@ def create_image(frame):
         data=rgb_frame
     )
 
+
+def compute_edges():
+    l_edge = ((avg_calibration[0].x) + (avg_calibration[2].x)) / 2
+    r_edge = ((avg_calibration[1].x) + avg_calibration[3].x) / 2
+
+    t_edge = (avg_calibration[0].y + avg_calibration[1].y) / 2
+    b_edge = (avg_calibration[2].y + avg_calibration[3].y) / 2
+    return l_edge, r_edge, t_edge, b_edge
+
+def draw_landmarks(frame, landmarks):
+    for landmark in EYE_MARKERS:
+        pass
+
+def calculate_smoothed_position(landmarks, previous_x, previous_y, w, h):
+    left_edge, right_edge, top_edge, bottom_edge = compute_edges()
+
+    lx, ly = get_center(LEFT_IRIS, landmarks[0], w, h)
+    rx, ry = get_center(RIGHT_IRIS, landmarks[0], w, h)
+    u = ((lx + rx) / 2 - left_edge) / (right_edge - left_edge)
+    v = ((ly + ry) / 2 - top_edge) / (bottom_edge - top_edge)
+    # Clamp values
+    u = max(0, min(1, u))
+    v = max(0, min(1, v))
+
+    target_x = u * SCREEN_WIDTH
+    target_y = v * SCREEN_HEIGHT
+
+    smooth_x = SMOOTHING * target_x + (1 - SMOOTHING) * previous_x
+    smooth_y = SMOOTHING * target_y + (1 - SMOOTHING) * previous_y
+    return smooth_x, smooth_y
+
 def main() -> None:
-    prev_x, prev_y = 0, 0
     flags = Flags()
+
+    prev_x, prev_y = 0, 0
 
     # --- Load model ---
     detector = create_detector()
-    print(type(detector))
 
     # --- Webcam ---
     cap = cv2.VideoCapture(CAMERA)
@@ -128,15 +155,14 @@ def main() -> None:
         frame = cv2.flip(frame, 1)
         h, w, _ = frame.shape
 
-        # Timestamp in milliseconds
+        # Timestamp
         timestamp = int(time.time() * 1000)
 
         # Run detection
         result = detector.detect_for_video(create_image(frame), timestamp)
 
-        # --- Draw landmarks ---
         if result.face_landmarks:
-            h, w, _ = frame.shape
+            # --- Draw landmarks ---
 
             # Full face
             """for landmark in result.face_landmarks[0]:
@@ -179,49 +205,19 @@ def main() -> None:
             cv2.circle(frame, (get_center(LEFT_IRIS,result.face_landmarks[0], w, h)), 1, (0, 255, 0), -1)
             cv2.circle(frame, (get_center(RIGHT_IRIS, result.face_landmarks[0], w, h)), 1, (0, 255, 0), -1)
 
-            # left eyelid points
-            #cv2.circle(frame, (int(result.face_landmarks[0][159].x * w), int(result.face_landmarks[0][159].y * h)), 1, (0, 255, 0), -1)
-            #cv2.circle(frame, (int(result.face_landmarks[0][145].x * w), int(result.face_landmarks[0][145].y * h)), 1, (0, 255, 0), -1)
 
+            left_EAR = calculate_EAR(result.face_landmarks[0], LEFT_EYE_CORNERS, LEFT_EYE_LIDS)
+            right_EAR = calculate_EAR(result.face_landmarks[0], RIGHT_EYE_CORNERS, RIGHT_EYE_LIDS)
+            # Convert eye position to screen position
+            if len(avg_calibration) == 4 and not flags.calibrating:
+                smooth_x, smooth_y = calculate_smoothed_position(result.face_landmarks, prev_x, prev_y, w, h)
+                prev_x, prev_y = smooth_x, smooth_y
 
-        # Show window
-        cv2.imshow("Face Landmarker", frame)
+                both_closed = left_EAR < WINK_THRESHOLD and right_EAR < WINK_THRESHOLD
+                if not both_closed:  # Dont moving during blinking
+                    move_mouse(smooth_x, smooth_y)
 
-        # Convert eye position to screen position
-        if not flags.calibrating and len(avg_calibration) == 0:
-            print(f"calibrating: {flags.calibrating}")
-        elif len(avg_calibration) != 0 and not flags.calibrating:
-            left_edge = ((avg_calibration[0].x) + (avg_calibration[2].x)) / 2
-            right_edge = ((avg_calibration[1].x) + avg_calibration[3].x) / 2
-
-            top_edge = (avg_calibration[0].y + avg_calibration[1].y) / 2
-            bottom_edge = (avg_calibration[2].y + avg_calibration[3].y) / 2
-
-            lx, ly = get_center(LEFT_IRIS,result.face_landmarks[0], w, h)
-            rx, ry = get_center(RIGHT_IRIS,result.face_landmarks[0], w, h)
-            u = ((lx + rx) / 2 - left_edge) / (right_edge - left_edge)
-            v = ((ly + ry) / 2 - top_edge) / (bottom_edge - top_edge)
-            # Clamp values
-            u = max(0, min(1, u))
-            v = max(0, min(1, v))
-
-            target_x = u * SCREEN_WIDTH
-            target_y = v * SCREEN_HEIGHT
-
-            smooth_x = SMOOTHING * target_x + (1-SMOOTHING) * prev_x
-            smooth_y = SMOOTHING * target_y + (1-SMOOTHING) * prev_y
-            prev_x, prev_y = smooth_x, smooth_y
-
-            left_EAR = calculate_EAR(result.face_landmarks[0], 33, 159, 145, 133)
-            right_EAR = calculate_EAR(result.face_landmarks[0], 362, 386, 374, 263)
-            both_closed = left_EAR < WINK_THRESHOLD and right_EAR < WINK_THRESHOLD
-            if not both_closed: # Dont moving during blinking
-                move_mouse(smooth_x, smooth_y)
-        #TODO: this if statement is redundant
-        if result.face_landmarks:
-            left_EAR = calculate_EAR(result.face_landmarks[0], 33, 159, 145, 133)
-            right_EAR = calculate_EAR(result.face_landmarks[0], 362, 386, 374, 263)
-            #FIXME: flag resets if you open one eye
+            # Winking check
             if left_EAR < WINK_THRESHOLD < right_EAR:
                 flags.wink_frames += 1
                 if flags.wink_frames == WINK_FRAMES_REQUIRED:
@@ -236,19 +232,19 @@ def main() -> None:
                 flags.wink_frames = 0
                 flags.right_wink_frames = 0
 
+        # Show window
+        cv2.imshow("Face Landmarker", frame)
 
+        #Keyboard inputs
         key = cv2.waitKey(1) & 0xFF
-        # Press ESC to quit
-        if key == 27:
+        if key == 27: # Quit
             break
-        elif key == ord("c") and flags.calibrating:
+        elif key == ord("c") and flags.calibrating: #Add calibration point
             if result.face_landmarks:
                 x, y = get_center(RIGHT_IRIS,result.face_landmarks[0], w, h)
                 right_cord = Coordinate(x, y)
-                right_calibration.append(right_cord)
                 left_x, left_y = get_center(LEFT_IRIS,result.face_landmarks[0],w, h)
                 left_cord = Coordinate(left_x, left_y)
-                left_calibration.append(left_cord)
                 avg_calibration.append(avg_coords(left_cord, right_cord))
                 if len(avg_calibration) >= 4:
                     flags.calibrating = False
@@ -266,7 +262,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-    """try:
-        main()
-    except Exception as e:
-        print("Error: {}".format(e))"""
